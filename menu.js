4c3a1fea3e107f069e444a0d6cccf145e4bbad42
Ari Ardiansyah <19242125@bsi.ac.id>
2025-06-12 11:18:20 +0700

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,52 @@
+const mobileMenuBtn = document.getElementById("mobile-menu-btn");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  if (mobileMenuBtn && mobileMenu) {
+    mobileMenuBtn.addEventListener("click", () => {
+      mobileMenu.classList.toggle("hidden");
+    });
+  }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll("nav a");
+
+const observer = new IntersectionObserver(
+    entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+        navLinks.forEach(link => {
+            link.classList.remove("active", "text-white");
+            link.classList.add("text-gray-300");
+            if (link.getAttribute("href") === `#${entry.target.id}`) {
+            link.classList.add("active", "text-white");
+            link.classList.remove("text-gray-300");
+            }
+        });
+        }
+    });
+    },
+    {
+    threshold: 0.5, // bagian tengah section terlihat minimal 50%
+    }
+);
+
+sections.forEach(section => observer.observe(section));
+});
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#000000',
+                secondary: '#6b21a8',
+                accent: '#9333ea',
+                highlight: '#a855f7'
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+        }
+    }
+}
